90a66aa3cd46d6c7886f1667bb12bf01bc9af8d1
jaapiyo <jaap@blinkinglights.nl>
2017-05-01 13:48:47 +0200

    Beetje netter gemaakt nadat jelle viezigheid achterliet

--- test.py
+++ test.py
@@ -1,21 +1,14 @@
 print("Hello World")
 print("test")
 
-memory
+# memory
 # aanpassing Jaap
 
 
 # aanpassing Jaap
 print ("Hallo Jaap")
 print("Zander houd van braai!")
-<<<<<<< HEAD
 print ("dit is van Jelle")
-=======
 
 
 # Dag meneer Tuk -Jaap
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
-
->>>>>>> origin/master
